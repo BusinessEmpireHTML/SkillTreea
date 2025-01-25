afa3e8cd048b5feeddb538aad33f8b7b86253051
BusinessEmpireHTML <businessempire767@gmail.com>
2025-01-25 16:05:38 -0600

    Delete demo directory

--- demo/demo.js
+++
@@ -1,8 +0,0 @@
-import { SkillTree } from '../skilltree.js';
-import { skillConfig } from './config.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const skillTree = new SkillTree(skillConfig);
-  // Mount the skill tree to the container and template
-  skillTree.mount('.skills-container', '#skill-template');
-});
